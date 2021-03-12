bee3f6595f0dc1552c3454f83f896ac20b3d7e4f
Gerry Andika Chandra <gergerchandra@gmail.com>
2021-03-13 01:41:29 +0800

    [Gerry] Fixing config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config()
 
 const { DB_USERNAME,
@@ -36,39 +35,3 @@ module.exports = {
     }
   }
 }
-=======
-require('dotenv').config();
-
-const { DB_USERNAME,
-  DB_PASSWORD,
-  DB_DATABASE,
-  DB_HOST,
-  DB_DIALECT,
-  DB_PORT
-} = process.env
-
-module.exports = {
-  "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_DATABASE,
-    "host": DB_HOST,
-    "dialect": DB_DIALECT,
-    "port": DB_PORT
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
->>>>>>> feature/categories-goals-milestone
