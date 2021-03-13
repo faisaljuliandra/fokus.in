295683a6f6fc56a8f49852879541f09fab8f4c71
Faisal Juliandra <faisaljuliandra@gmail.com>
2021-03-13 20:00:18 +0700

    fix goals seeders

--- seeders/20210313115654-Goals.js
+++ seeders/20210313115654-Goals.js
@@ -6,7 +6,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Goals', [
       { id: nanoid(), categoriesId: '-75aoJC7hwadrknBNLu3q', namaGoal: 'Menjadi UI/UX Designer', deskripsiGoal: 'Belajar menjadi UI/UX', estimationTime: '1 tahun', createdAt: new Date(), updatedAt: new Date() },
-      { id: nanoid(), categoriesId: '-75aoJC7hwadrknBNLu3q', namaGoal: 'Menjadi Freelance Graphic Designer', deskripsiGoal: 'Belajar menjadi Graphic Designer', estimationTime: '6 bulan', createdAt: new Date(), updatedAt: new Date() },
+      { id: nanoid(), categoriesId: '-75aoJC7hwadrknBNLu3q', namaGoal: 'Menjadi Graphic Designer', deskripsiGoal: 'Belajar menjadi Graphic Designer', estimationTime: '6 bulan', createdAt: new Date(), updatedAt: new Date() },
       { id: nanoid(), categoriesId: 'E0J98r_xTXgTOevS2PbYk', namaGoal: 'Menjadi Youtuber', deskripsiGoal: 'Belajar menjadi youtuber', estimationTime: '6 bulan', createdAt: new Date(), updatedAt: new Date() },
       { id: nanoid(), categoriesId: 'uGX0qsaTIx2s_3MaQTm5L', namaGoal: 'Membuat Aplikasi Mobile', deskripsiGoal: 'Belajar membuat aplikasi mobile', estimationTime: '1 tahun', createdAt: new Date(), updatedAt: new Date() },
       { id: nanoid(), categoriesId: 'uGX0qsaTIx2s_3MaQTm5L', namaGoal: 'Membuat Start-Up', deskripsiGoal: 'Belajar mengenai startup', estimationTime: '2 tahun', createdAt: new Date(), updatedAt: new Date() },
