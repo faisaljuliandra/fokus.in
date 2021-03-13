6c75ac57b5e339b57d62c10baba42789953814ae
Faisal Juliandra <faisaljuliandra@gmail.com>
2021-03-13 11:08:59 +0700

    fix validator

--- validator/validators.js
+++ validator/validators.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { body } = require('express-validator');
 
 exports.validateRegister = [
@@ -17,23 +16,3 @@ exports.validateLogin = [
     body('password', 'Cannot be empty').notEmpty(),
     body('password', 'Must a string').isString()
 ]
-=======
-const { body } = require('express-validator');
-
-exports.validateRegister = [
-    body('username', 'Cannot be empty').notEmpty(),
-    body('username', 'Must a string').isString(),
-    body('username', 'Must be minimal 6 characters').isLength({ min: 6 }),
-    body('username', 'Must be maximal 12 characters').isLength({ max: 12 }),
-    body('password', 'Cannot be empty').notEmpty(),
-    body('password', 'Must be minimal 8 characters').isLength({ min: 8 }),
-    body('password', 'Must be maximal 16 characters').isLength({ max: 16 })
-]
-
-exports.validateLogin = [
-    body('username', 'Cannot be empty').notEmpty(),
-    body('username', 'Must a string').isString(),
-    body('password', 'Cannot be empty').notEmpty(),
-    body('password', 'Must a string').isString()
-]
->>>>>>> feature/categories-goals-milestone
