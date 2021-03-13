a84ba2cf6eaf4656112e85f9fd6b009b65edba1e
Faisal Juliandra <faisaljuliandra@gmail.com>
2021-03-14 02:47:57 +0700

    fix discussion filter by goalId

--- controllers/discussionsController.js
+++ controllers/discussionsController.js
@@ -1,10 +1,21 @@
-const {Discussions} = require('../models')
+const {Discussions, Goals} = require('../models')
 const {nanoid} = require('nanoid')
 const BaseController = require('./baseController')
 
 class DiscussionsController extends BaseController{
     constructor(){
         super(Discussions)
+    }
+
+    findByGoalsId(goal) {
+        return Discussions.findAll({
+            include: {
+                model: Goals,
+                where: {
+                    ...goal && { id: goal }
+                }
+            }
+        })
     }
 }
 
