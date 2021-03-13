d50a5b7d4ecc96d99ba8ffab97242c887b9011f1
Faisal Juliandra <faisaljuliandra@gmail.com>
2021-03-13 11:08:37 +0700

    update seeders data

--- seeders/20210310134517-categories.js
+++ seeders/20210310134517-categories.js
@@ -8,7 +8,7 @@ module.exports = {
       { id: nanoid(), namaKategori: 'Bisnis & Keuangan', createdAt: new Date(), updatedAt: new Date() },
       { id: nanoid(), namaKategori: 'Teknologi', createdAt: new Date(), updatedAt: new Date() },
       { id: nanoid(), namaKategori: 'Desain', createdAt: new Date(), updatedAt: new Date() },
-      { id: nanoid(), namaKategori: 'Film & Video', createdAt: new Date(), updatedAt: new Date() },
+      { id: nanoid(), namaKategori: 'Entertainment', createdAt: new Date(), updatedAt: new Date() },
       { id: nanoid(), namaKategori: 'Pengembangan Diri', createdAt: new Date(), updatedAt: new Date() }
     ])
   },
