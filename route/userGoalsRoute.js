a01e1a0fba95b5afa72217535bb46a09ef40cc30
Gerry Andika Chandra <gergerchandra@gmail.com>
2021-03-14 23:52:21 +0800

    [Gerry] fixing file

--- route/userGoalsRoute.js
+++ route/userGoalsRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const userGoalsController = require('../controllers/usergoalscontroller')
+const userGoalsController = require('../controllers/usergoalscontrol')
 const passport = require('../middleware/passportMiddleware')
 const verifyRole = require('../middleware/roleMiddleware')
 
