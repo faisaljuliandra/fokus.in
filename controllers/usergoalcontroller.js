227f5b810195503a25b2b9819d72426b2b5b3af6
Gerry Andika Chandra <gergerchandra@gmail.com>
2021-03-14 17:40:39 +0800

    [Gerry] Fixing error

--- controllers/usergoalcontroller.js
+++ controllers/usergoalcontroller.js
@@ -1,7 +1,6 @@
 const { UserGoals, Users, Goals } = require('../models')
 const {nanoid} = require('nanoid')
 const BaseController = require('./baseController')
-const sequelize = require('sequelize')
 
 class UserGoalsController extends BaseController{
     constructor(){
@@ -26,23 +25,6 @@ class UserGoalsController extends BaseController{
             }]
         })
     }
-
-
-    countTopGoals() {
-        return UserGoals.findAll({
-            attributes: [[sequelize.fn('count', sequelize.col('UserGoals.id')), 'count']],
-            order: sequelize.literal('count DESC'),
-            where: {
-                isEnrolled: true
-            },
-            include: [{
-                attributes: ['namaGoal', 'id'],
-                model: Goals
-            }],
-            group: ['Goal.id', 'Goal.namaGoal'],
-            limit: 3
-        });
-    }
-
 }
+
 module.exports = UserGoalsController
